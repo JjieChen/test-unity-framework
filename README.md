a07328b93311e93d1a258eb0b486f5e6b61165f7
Forest <linshs@qq.com>
2017-05-10 11:53:06 +0800

    添加工程

---
+++ README.md
@@ -0,0 +1,6 @@
+## 测试 Unity-iOS导出的framework
+
+这个xcode工程用于测试Unity-iOS导出的framework是否可以正常使用。
+
+**注意：Unity-iOS导出的framework 在[unity-ios-framework](https://gitlab.com/octten/unity-ios-framework)工程中叫做 `UnityOutFramework.framework`，文件很大，不放在 git 管理中。需要在[unity-ios-framework](https://gitlab.com/octten/unity-ios-framework)工程中生成，然后拖拽放入本工程中即可。**
+
---
+++ unity-framework-test/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  unity-framework-test
+//
+//  Created by forest on 2017/5/8.
+//  Copyright © 2017年 vxinyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UnityOutFramework/UnityOutFramework.h>
+#include <mach/mach_time.h>
+#import "AppDelegate.h"
+
+//void UnityInitTrampoline();
+
+int main(int argc, char * argv[]) {
+    
+    signed long long startTime = mach_absolute_time();
+    
+    @autoreleasepool {
+        UnitySetStartupTime(startTime);
+        //UnityInitTrampoline();
+        UnityInitRuntime(argc, argv);
+        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
